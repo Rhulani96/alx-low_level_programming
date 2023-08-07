a8b2abfc8881965b385923084875b651f6012f56
Rhulani96 <rhulanikhenso80@gmail.com>
2023-08-07 11:26:42 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_Buffer(char *file);
+char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_Buffer - Allocates 1024 bytes for a buffer.
+ * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
